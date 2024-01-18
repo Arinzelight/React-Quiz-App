69e8e14990c6dd18b2629e49fc6e52780670bddc
Arinzelight <arinzelight2@gmail.com>
2024-01-18 20:33:29 +0100

    render questions

--- src/pages/quiz/Quiz.jsx
+++ src/pages/quiz/Quiz.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useEffect } from "react";
 
-const Quiz = () => {
-  return <div>Quiz</div>;
+const Quiz = ({ name, score, questions, setQuestions, setScore }) => {
+  useEffect(() => {
+    console.log(questions);
+  }, [questions]);
+
+  return <div>qst</div>;
 };
 
 export default Quiz;
