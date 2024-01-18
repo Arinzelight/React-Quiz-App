559d2caa1a01b659429c5f9a3ba520beed8335e7
Arinzelight <arinzelight2@gmail.com>
2024-01-18 21:16:54 +0100

    circular progress for questions

--- src/pages/quiz/Quiz.jsx
+++ src/pages/quiz/Quiz.jsx
@@ -1,12 +1,44 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const Quiz = ({ name, score, questions, setQuestions, setScore }) => {
+  const [options, setOptions] = useState();
+  const [currQuest, setCurrQuest] = useState(0);
+
   useEffect(() => {
     console.log(questions);
+
+    setOptions(
+      questions &&
+        handleShuffle([
+          questions[currQuest]?.correct_answer,
+          ...questions[currQuest]?.incorrect_answers,
+        ])
+    );
   }, [questions]);
 
-  return <div>qst</div>;
+  console.log(options);
+
+  const handleShuffle = (optionss) => {
+    return optionss.sort(() => Math.random() - 0.5);
+  };
+
+  return (
+    <div>
+      <span className="subtitle">Welcome {name}</span>
+      {questions ? (
+        <>Questions</>
+      ) : (
+        <CircularProgress
+          style={{ margin: 100 }}
+          color="primary"
+          size={150}
+          thickness={1}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Quiz;
